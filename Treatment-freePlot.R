857051077b3b66ef8e8ebc8455aa3909cdc8debd
Cong Jiang <cjiang080892@gmail.com>
2024-03-20 22:40:04 -0400

    Update Treatment-freePlot.R

--- Treatment-freePlot.R
+++ Treatment-freePlot.R
@@ -9,3 +9,16 @@ lines(x[j],l1[j],col="blue",lwd=2)
 legend(0.3, 8, legend=c("Treatment-free function", "Linear regression"),
        col=c("red", "blue"), lty=1, cex=1,
        box.lty=2)
+
+
+############
+set.seed(1)
+x <- runif(100, -0.5,0.5)
+y <- x + exp(x) + cos(pi*x) + x^3 + rnorm(100)
+plot(y ~ x, main = "Treatment-free function and its linear regression",  ylab = "f(x)")
+curve(x + exp(x) + cos(pi*x) + x^3, add = TRUE, col = "red",lwd=2)
+l1<-predict(lm(y ~ x))
+lines(x[j],l1[j],col="blue",lwd=2)
+legend(0.3, 8, legend=c("Treatment-free function", "Linear regression"),
+       col=c("red", "blue"), lty=1, cex=1,
+       box.lty=2)
